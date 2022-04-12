2ef935b09281a12e5f0f50c1b4dbd7978f690628
da99 <1551848+da99@users.noreply.github.com>
2022-04-11 21:40:27 -0500

    Merged: README into code.

--- middlewares/cross-origin-resource-policy.ts
+++ middlewares/cross-origin-resource-policy.ts
@@ -1,4 +1,20 @@
 
+// # Cross-Origin-Resource-Policy middleware
+//
+// This middleware sets the `Cross-Origin-Resource-Policy` header. Read about it [in the spec](https://resourcepolicy.fyi/).
+//
+// Usage:
+//
+// ```typescript
+// import {cross_origin_resource_policy} from "https://raw.githubusercontent.com/da99/helmet/main/middlewares/cross-origin-resource-policy.ts";
+//
+// // Sets "Cross-Origin-Resource-Policy: same-origin"
+// cross_origin_resource_policy(my_response)
+// cross_origin_resource_policy(my_response, "same-origin")
+//
+// // Sets "Cross-Origin-Resource-Policy: same-site"
+// cross_origin_resource_policy(my_response, "same-site")
+// ```
 
 export type ALLOWED_POLICIES = "same-origin" | "same-site" | "cross-origin";
 
--- middlewares/referrer-policy.ts
+++ middlewares/referrer-policy.ts
@@ -1,45 +1,39 @@
-export interface ReferrerPolicyOptions {
-  policy?: string | string[];
-}
+// # Referrer-Policy middleware
+//
+// The [Referer HTTP header](https://en.wikipedia.org/wiki/HTTP_referer) is typically set by web browsers to tell the server where it's coming from. For example, if you click a link on _example.com/index.html_ that takes you to _wikipedia.org_, Wikipedia's servers will see `Referer: example.com`. This can have privacy implications—websites can see where you are coming from. The new [`Referrer-Policy` HTTP header](https://www.w3.org/TR/referrer-policy/#referrer-policy-header) lets authors control how browsers set the Referer header.
+//
+// [Read the spec](https://www.w3.org/TR/referrer-policy/#referrer-policies) to see the options you can provide.
+//
+// Usage:
+//
+// ```typescript
+// import {referrer_policy} from "https://raw.githubusercontent.com/da99/helmet/main/middlewares/referrer_policy.ts";
+//
+// referrer_policy(my_response, "same-origin");
+// // Referrer-Policy: same-origin
+//
+// referrer_policy(my_response, "unsafe-url");
+// // Referrer-Policy: unsafe-url
+//
+// referrer_policy(my_response, "no-referrer", "unsafe-url")
+// // Referrer-Policy: no-referrer,unsafe-url
+//
+// referrer_policy(my_response);
+// // Referrer-Policy: no-referrer
+// ```
 
-const ALLOWED_TOKENS = new Set<string>([
-  "no-referrer",
-  "no-referrer-when-downgrade",
-  "same-origin",
-  "origin",
-  "strict-origin",
-  "origin-when-cross-origin",
-  "strict-origin-when-cross-origin",
-  "unsafe-url",
-  "",
-]);
 
-export function get_header_value_from_options({ policy = ["no-referrer"], }: Readonly<ReferrerPolicyOptions>): string {
-  const tokens = typeof policy === "string" ? [policy] : policy;
+export type ALLOWED_TOKENS = "no-referrer" | 
+  "no-referrer-when-downgrade" | "same-origin" | 
+  "origin" | "strict-origin" | 
+  "origin-when-cross-origin" | 
+  "strict-origin-when-cross-origin" | "unsafe-url";
 
-  if (tokens.length === 0) {
-    throw new Error("Referrer-Policy received no policy tokens");
-  }
 
-  const tokensSeen = new Set<string>();
-  tokens.forEach((token) => {
-    if (!ALLOWED_TOKENS.has(token)) {
-      throw new Error(
-        `Referrer-Policy received an unexpected policy token ${JSON.stringify(token)}`
-      );
-    } else if (tokensSeen.has(token)) {
-      throw new Error(
-        `Referrer-Policy received a duplicate policy token ${JSON.stringify(token)}`
-      );
-    }
-    tokensSeen.add(token);
-  });
-
-  return tokens.join(",");
-} // export function
-
-export function referrer_policy(r: Response, options: Readonly<ReferrerPolicyOptions> = {}) {
-  r.headers.set("Referrer-Policy", get_header_value_from_options(options));
+export function referrer_policy(r: Response, ...options: ALLOWED_TOKENS[]) {
+  if (options.length === 0)
+    options.push("no-referrer");
+  r.headers.set("Referrer-Policy", options.join(", "));
   return r;
 } // export function
 
